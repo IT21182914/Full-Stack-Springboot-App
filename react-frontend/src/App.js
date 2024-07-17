a9f959c41546930e2eae2e617e2a7ae705154838
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-07-17 10:59:50 +0530

    frontend

---
+++ react-frontend/src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./layout/Navbar";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddUser from "./users/AddUser";
+import EditUser from "./users/EditUser";
+import ViewUser from "./users/ViewUser";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/adduser" element={<AddUser />} />
+          <Route path="/edituser/:id" element={<EditUser />} />
+          <Route path="/viewuser/:id" element={<ViewUser />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
